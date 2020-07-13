c83e2d9ce4dbc129e7bbf31f371e28482e46f734
Achim Munene <achim_munene@hotmail.com>
2020-07-13 22:43:48 +0200

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 import mongodb from 'mongodb';
 
 export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
-  "bodyLimit": "100kb"
+    // "port": 3005,
+    // "mongoUrl": "mongodb://localhost:27017/chat-api",
+    "port": process.env.PORT,
+    "mongoUrl": "mongodb+srv://mongo:<testpassword>@chatty.hhgh4.mongodb.net/<chatty>?retryWrites=true&w=majority",
+    "bodyLimit": "100kb"
 }
